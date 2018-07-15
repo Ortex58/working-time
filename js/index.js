3e2a5d230184d8f1ab9ca844934d42dfdffe76a4
Ortex <ortex@i.ua>
2018-07-15 19:51:15 +0000

    add reset

--- js/index.js
+++ js/index.js
@@ -5,39 +5,49 @@ function convert(a){
 	  console.log(hours +":"+ minutes);
 	}
 var min = document.querySelector('#min'),
-	minutes, hours,	a,
-	send = document.querySelector('#send'),
-	result = document.querySelector('#res'),
-	hour = 60,
-	res = document.querySelector('#res');
-	
+		 send = document.querySelector('.send'),
+		 reset = document.querySelector('.reset'),
+		 result = document.querySelector('#hour'),
+		 hour = 60;
 
-min.focus();
+		 min.focus();
+		 result.value = '0' + ':' + '00';
 
-send.addEventListener('click', function (e) {
- e.preventDefault();
- minutes = min.value;
-
- //Рахуємо кількість годин
-if ( minutes < hour ) {
-		return result.value = minutes + ' ' + 'min';
-} else {
-		a = minutes; //Берем значення поля min
-		convert(a); // надаєм фунцкції значення поля мін зі змінної а
-		//Вивід результату
-		if (minutes < 10 ) {
-			result.value = hours + ':' + 0 + minutes;
-		} else {
-			result.value = hours + ':' + minutes;
+		 send.addEventListener('click', function (e) {
+		 	e.preventDefault();
+		 	minutes = min.value;
+ 			if (~minutes.indexOf('.') || minutes < 0) {
+			  result.value = 'Error';
+			} else
+		 	//Рахуємо кількість годин
+			if ( minutes < hour ) {
+					return result.value = minutes + ' ' + 'min';
+			} else if ( minutes == '') {
+				result.value = '0' + 'min';
+			} else {
+				a = minutes; //Берем значення поля min
+				convert(a); // надаєм фунцкції значення поля мін зі змінної а
+				//Вивід результату
+				if (minutes < 10 ) {
+					result.value = hours + ':' + 0 + minutes;
+				} else {
+					result.value = hours + ':' + minutes;
+				}
 			}
-	}
-});
+		 });
+		 //Подія reset для кнопки reset
+		 reset.addEventListener('click', function (e) {
+		 	e.preventDefault();
+		 	min.value = '';
+		 	result.value = '0' + ':' + '00';
+		 	min.focus();
+		 });
 
 
 /*for (i=1; i<=24; i++) {
-	s = hour * i;
-	if ( s <= min ) {
-		god = i;
-		hvy = min - s;//Рахуємо кількісто хвилин
-	}
-}*/
+					s = hour * i;
+					if ( s <= min ) {
+						god = i;
+						hvy = min - s;//Рахуємо кількісто хвилин
+					}
+				}*/
